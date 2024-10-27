ca976482cdf3e8dc91a28ed9c2110a20a518a0a0
KDev <kirujsx@gmail.com>
2024-10-26 21:40:10 -0400

    Add files via upload

---
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+import Banner from "../../components/Banner/Banner";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import RowList from "../../components/Rows/RowList/RowList";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowList/>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
